1c5db840bb2ec05dcbc4d21527e3da8978294ad7
AxelDuval <axel.duval@gmail.com>
2021-11-26 11:53:23 +0100

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,30 @@
-function Contact(){
+import './animations.css';
+import './Bio.css';
+
+import { Container, Tab } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Image } from 'react-bootstrap';
+
+function Contact() {
     return (
-        <h1>Contactez-moi</h1>
+        <Container fluid className="portfolio text-center mt-5">
+            <Row>
+                <Col> <div className="text-white"><h1 className="tracking-in-expand poppinsTitle">Me contacter</h1></div></Col>
+            </Row>
+            <Row>
+                <Col> <div className="text-white mt-3">
+                    <p className="tracking-in-expand poppinsText"> Pour m'envoyer un petit message c'est par ici !
+                    </p>
+                    <div>
+                        <a className="text-white tracking-in-expand poppinsText" HREF="mailto:axel.duval@gmail.com">axel.duval@gmail.com</a>
+                    </div>
+
+                </div>
+                </Col>
+            </Row>
+
+        </Container>
     )
 }
 
